b6364a325c8e1f5e1a6a436125c6f859104e608f
Zaid9498 <136815199+Zaid9498@users.noreply.github.com>
2023-08-25 22:37:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const removeButton = document.getElementById("removeButton");
+const colorSelect = document.getElementById("colorSelect");
+
+removeButton.addEventListener("click", function() {
+  const selectedIndex = colorSelect.selectedIndex;
+  if (selectedIndex !== -1) {
+    colorSelect.options[selectedIndex].remove();
+  }
+});
