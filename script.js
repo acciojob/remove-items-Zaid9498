3e2356ca5f20e306d8948383a2344303a0108fc8
Zaid9498 <136815199+Zaid9498@users.noreply.github.com>
2023-08-25 23:06:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let btn=document.getElementsByTagName("input")[0];
 let colorSelect=document.querySelector("#colorSelect");
-let call=function(){
-	colorSelect.remove();
+
+	let call = function() {
+    colorSelect.remove(colorSelect.selectedIndex);
+}
 }
 
 btn.addEventListener("click",call);
